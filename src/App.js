a37975642ba7d8ddc084efee2ed4599af40e72e9
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 21:24:15 -0500

    Set counters Import useState and useCallback Create buttons to increment counters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState, useCallback } from 'react';
 
 function App() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  const increment1 = () => {
+    setCounter1(counter1 + 1);
+  }
+  
+  const increment2 = () => {
+    setCounter2(counter2 + 1);
+  }
+
+
   return (
     <div className="App">
+      <p>Counter 1: { counter1 } Counter 2: { counter2 }</p>
+      <button onClick={ increment1 } >Increment counter 1</button>
+      <button onClick={ increment2 } >Increment counter 2</button>
     </div>
   );
 }
